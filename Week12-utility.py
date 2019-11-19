fb191b7c5b349637119649953b9b73ec48cbd031
Renata Boyd <rboyd1@mymail.mines.edu>
2019-11-18 22:04:35 -0700

    commit 3

--- Week12-utility.py
+++ Week12-utility.py
@@ -4,3 +4,10 @@
 
 def PrintOutput(my_input):
     return print('OUTPUT', my_input)
+
+def LoadFile(filename):
+    file = open(filename, 'r')
+    string = file.read().splitlines()
+    return string
+    file.close()
+
